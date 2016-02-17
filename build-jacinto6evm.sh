7cec59a0fce2c35a5dc9cec2842ee5878bf662db
Robert Nelson <robertcnelson@gmail.com>
2016-02-16 22:14:12 -0600

    ti: ti-img-sgx/1.14.3699939/k4.1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ build-jacinto6evm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ARCH=$(uname -m)
+device="jacinto6evm"
+branch="ti-img-sgx/1.14.3699939/k4.1"
+
+if [ -f .builddir ] ; then
+	if [ -d ./src ] ; then
+		rm -rf ./src || true
+	fi
+
+	git clone -b ${branch} https://github.com/rcn-ee/ti-omap5-sgx-ddk-linux ./src --depth=1
+
+	x86_dir="/opt/github/bb.org/4.1-normal"
+	x86_compiler="gcc-linaro-4.9-2015.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-"
+
+	if [ "x${ARCH}" = "xarmv7l" ] ; then
+		make_options="CROSS_COMPILE= KERNELDIR=/build/buildd/linux-src"
+	else
+		make_options="CROSS_COMPILE=/home/voodoo/dl/gcc/${x86_compiler} KERNELDIR=${x86_dir}/KERNEL"
+	fi
+
+	cd ./src/eurasia_km/eurasiacon/build/linux2/omap_linux
+
+	make ARCH=arm ${make_options} clean
+	make ARCH=arm ${make_options}
+fi
+#
---
+++ package-ti335x.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e
+
+ARCH=$(uname -m)
+
+if [ "x${ARCH}" = "xarmv7l" ] ; then
+	uname_r="VERSION"
+	distro="DISTRO"
+	dpkg_arch="armhf"
+else
+	uname_r="4.1.18-ti-r48.3"
+	distro="jessie"
+	dpkg_arch="amd64"
+fi
+
+package="ti-sgx"
+base_dir="./"
+
+device="ti335x"
+
+if [ -f ${base_dir}/src/eurasia_km/eurasiacon/binary2_omap_linux_release/target/kbuild/pvrsrvkm.ko ] ; then
+
+	#modules
+	cp -v ${base_dir}/src/eurasia_km/eurasiacon/binary2_omap_linux_release/target/kbuild/pvrsrvkm.ko ${base_dir}
+	cp -v ${base_dir}/src/eurasia_km/eurasiacon/binary2_omap_linux_release/target/kbuild/bc_example.ko ${base_dir}
+
+	#readme
+	cp -v ${base_dir}/src/eurasia_km/README ${base_dir}
+	cp -v ${base_dir}/src/eurasia_km/MIT-COPYING ${base_dir}
+
+	echo "Section: misc" > ${base_dir}control
+	echo "Priority: optional" >> ${base_dir}control
+	echo "Homepage: https://github.com/rcn-ee/${package}-omap5" >> ${base_dir}control
+	echo "Standards-Version: 3.9.6" >> ${base_dir}control
+	echo "" >> ${base_dir}control
+	echo "Package: ${package}-${device}-modules-${uname_r}" >> ${base_dir}control
+	echo "Version: 1${distro}" >> ${base_dir}control
+	echo "Pre-Depends: linux-image-${uname_r}" >> ${base_dir}control
+	echo "Depends: linux-image-${uname_r}" >> ${base_dir}control
+	echo "Maintainer: Robert Nelson <robertcnelson@gmail.com>" >> ${base_dir}control
+	echo "Architecture: ${dpkg_arch}" >> ${base_dir}control
+	echo "Copyright: MIT-COPYING" >> ${base_dir}control
+	echo "Readme: README" >> ${base_dir}control
+	echo "Files: pvrsrvkm.ko /lib/modules/${uname_r}/extra/${device}/" >> ${base_dir}control
+	echo " bc_example.ko /lib/modules/${uname_r}/extra/${device}/" >> ${base_dir}control
+	echo "Description: ${package} modules" >> ${base_dir}control
+	echo " Kernel modules for ${package} devices" >> ${base_dir}control
+	echo "" >> ${base_dir}control
+
+	equivs-build control
+
+	rm -rf pvrsrvkm.ko || true
+	rm -rf bc_example.ko || true
+	rm -rf README || true
+	rm -rf MIT-COPYING || true
+	rm -rf control || true
+
+fi
