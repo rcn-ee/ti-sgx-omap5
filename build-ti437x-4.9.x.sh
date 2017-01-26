b72fef3e0136b8da22c7fbf6f220633046f14bc4
Robert Nelson <robertcnelson@gmail.com>
2017-01-26 16:08:31 -0600

    4.9.x sgx modules
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ build-ti437x-4.9.x.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ARCH=$(uname -m)
+device="ti437x"
+branch="ti-img-sgx/1.14.3699939/k4.9"
+
+if [ -f .builddir ] ; then
+	if [ -d ./src ] ; then
+		rm -rf ./src || true
+	fi
+
+	git clone -b ${branch} https://github.com/rcn-ee/ti-omap5-sgx-ddk-linux ./src --depth=1
+	cd ./src/
+	patch -p1 < ../../0001-sgx-add-fno-PIE.patch
+	cd ../
+
+	x86_dir="/opt/github/bb.org/ti-4.9/normal"
+	x86_compiler="gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-"
+
+	if [ "x${ARCH}" = "xarmv7l" ] ; then
+		make_options="CROSS_COMPILE= KERNELDIR=/build/buildd/linux-src TARGET_PRODUCT=${device}"
+	else
+		make_options="CROSS_COMPILE=/home/voodoo/dl/gcc/${x86_compiler} KERNELDIR=${x86_dir}/KERNEL TARGET_PRODUCT=${device}"
+	fi
+
+	cd ./src/eurasia_km/eurasiacon/build/linux2/omap_linux
+
+	make ARCH=arm ${make_options} clean
+	echo "make ARCH=arm ${make_options}"
+	make ARCH=arm ${make_options}
+fi
+#
