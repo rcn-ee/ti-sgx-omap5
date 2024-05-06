ab1a8445a39d792b8b1801a2e0bb09ef5408eec9
Robert Nelson <robertcnelson@gmail.com>
2024-05-06 16:06:08 -0500

    arm64: add am62-6.1.x and j721e-6.1.x
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ build-am62-6.1.x.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#https://git.ti.com/gitweb?p=arago-project/meta-ti.git;a=tree;f=recipes-bsp/powervr-drivers;hb=refs/heads/dunfell
+
+ARCH=$(uname -m)
+device="am62"
+branch="linuxws/kirkstone/k6.1/23.3.6512818"
+
+#  LD [M]  /opt/github/buildscripts/bb.org/6.1.x/ti-sgx-omap5/src/src/binary_am62_linux_wayland_release/target_aarch64/kbuild/pvrsrvkm.ko
+
+if [ -f .builddir ] ; then
+	if [ -d ./src ] ; then
+		rm -rf ./src || true
+	fi
+
+	echo "git clone -b ${branch} https://github.com/rcn-ee/ti-img-rogue-driver ./src --depth=1"
+	git clone -b ${branch} https://github.com/rcn-ee/ti-img-rogue-driver ./src --depth=1
+	cd ./src/
+	echo "patch -p1 < ../../0001-buildvars.mk-add-fno-PIE.patch"
+	patch -p1 < ../../0001-buildvars.mk-add-fno-PIE.patch
+
+	git diff
+	cd ../
+
+	PVR_SOC=am62_linux
+	PVR_BUILD=release
+	PVR_WS=wayland
+	if [ "x${ARCH}" = "xaarch64" ] ; then
+		make_options="CROSS_COMPILE=aarch64-linux-gnu- KERNELDIR=/build/buildd/linux-src RGX_BVNC=22.104.208.318 BUILD=${PVR_BUILD} PVR_BUILD_DIR=${PVR_SOC} WINDOW_SYSTEM=${PVR_WS}"
+	else
+		x86_dir="`pwd`/../../normal-arm64"
+		if [ -f `pwd`/../../normal-arm64/.CC ] ; then
+			. `pwd`/../../normal-arm64/.CC
+			make_options="CROSS_COMPILE=aarch64-linux-gnu- KERNELDIR=${x86_dir}/KERNEL RGX_BVNC=22.104.208.318 BUILD=${PVR_BUILD} PVR_BUILD_DIR=${PVR_SOC} WINDOW_SYSTEM=${PVR_WS}"
+		fi
+
+#		x86_dir="`pwd`/../../rt"
+#		if [ -f `pwd`/../../rt/.CC ] ; then
+#			. `pwd`/../../rt/.CC
+#			make_options="CROSS_COMPILE=${CC} KERNELDIR=${x86_dir}/KERNEL TARGET_PRODUCT=${device}"
+#		fi
+	fi
+
+	#exit 2
+	cd ./src/build/linux/am62_linux
+	make ARCH=arm64 ${make_options} clean
+	echo "make ARCH=arm64 ${make_options}"
+	make ARCH=arm64 ${make_options}
+fi
+#
